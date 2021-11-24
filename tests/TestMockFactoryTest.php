13a355de0be09e691bb8ec3a5e67506608e11bbb
Guillaume Delré <delre.guillaume@gmail.com>
2021-11-24 14:18:00 +0100

    [phpunit-mock-factory] implement phpunit TestMockFactory

---
+++ tests/TestMockFactoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Agilicode\PhpUnitTestFactory;
+
+use Agilicode\PhpUnitTestFactory\TestMockFactory;
+use Agilicode\PhpUnitTestFactory\TestMockFactoryInterface;
+use PHPUnit\Framework\TestCase;
+
+class TestMockFactoryTest extends TestCase
+{
+    public function testCreate()
+    {
+        $mockFactory = TestMockFactory::create(TestMockFactoryInterface::class, [], $this);
+        $this->assertInstanceOf(TestMockFactoryInterface::class, $mockFactory->getObject());
+        $this->assertInstanceOf(TestMockFactoryInterface::class, $mockFactory->getObject());
+    }
+
+    public function testCreateWillThrowsReflectionException()
+    {
+        $this->expectException(\ReflectionException::class);
+        $this->expectExceptionCode(-1);
+        $this->expectExceptionMessage('Class className does not exist');
+        TestMockFactory::create('className', [], $this);
+    }
+}
