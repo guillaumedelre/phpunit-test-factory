aae7b236aab40b9cb0d1e83622436317fa3d8751
Guillaume Delré <delre.guillaume@gmail.com>
2021-11-24 22:20:18 +0100

    [phpunit-test-factory] refacto phpunit conf with new php image for evaluation

--- tests/TestMockFactoryTest.php
+++ tests/TestMockFactoryTest.php
@@ -11,7 +11,7 @@ class TestMockFactoryTest extends TestCase
     public function testCreate()
     {
         $mockFactory = TestMockFactory::create(TestMockFactoryInterface::class, [], $this);
-        $this->assertInstanceOf(TestMockFactoryInterface::class, $mockFactory->getObject());
+        $this->assertInstanceOf(TestMockFactoryInterface::class, $mockFactory);
         $this->assertInstanceOf(TestMockFactoryInterface::class, $mockFactory->getObject());
     }
 
